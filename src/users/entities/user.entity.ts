4b3e0ef8dfea49549650ba6e59f3ba5cbb4f07c6
Nicolas Cavasin <ncavasin97@gmail.com>
2021-11-16 14:43:23 -0300

    adding user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  dni: number;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ length: 100 })
+  email: string;
+
+  @Column({ default: true })
+  active: boolean;
+}
