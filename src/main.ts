6f803771add937dceebdf84b65e5cf2317b15719
Nicolas Cavasin <ncavasin97@gmail.com>
2021-11-17 10:13:39 -0300

    adding catch block to bootstrap in order to comply with nest error

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
+const microserviceOptions = {
+  transport: Transport.GRPC,
+  options: {
+    url: '0.0.0.0:50051',
+    package: 'proto',
+    protoPath: join(process.cwd(), '/src/proto/users.proto'),
+  },
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(30000);
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    microserviceOptions,
+  );
+  await app.listen();
 }
-bootstrap();
+
+bootstrap().catch((e) => console.log(`${e}`));
